6fd60815b6bb9fe647752026bc1374b9938d7ce5
java80 <iekedede@hotmail.com>
2021-04-16 17:07:25 -0400

    fixed typo in the routes/users.js

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,5 @@
 const { Router} = require("express");
 const { signUp } = require("../controllers/users.js");
 const router = Router();
-router.post("/sign-up", signUp);∫
-module.exports = router;
+router.post("/sign-up", signUp);
+module.exports = router; 
