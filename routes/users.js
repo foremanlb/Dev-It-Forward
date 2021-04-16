effce3350ba9f0704771151c208fd178e9bb7948
java80 <iekedede@hotmail.com>
2021-04-16 15:42:21 -0400

    User.js route done

---
+++ routes/users.js
@@ -0,0 +1,8 @@
+const { Router, Router } = require("express");
+const { signUp, signUp, verify } = require("../controllers/users.js");
+const router = Router();
+router.post("/sign-up", signUp);
+router.post("/sign-in", signIn);
+router.get("/verify", verify);
+
+module.exports = router;
