215937dbf4cabf14a6206f55812889313a2f5ef5
Craig Navarro <craignavarro@Craigs-MacBook-Pro.local>
2021-04-16 14:08:00 -0700

    controllers users boilerplate

--- controllers/users.js
+++ controllers/users.js
@@ -1,3 +1,10 @@
+const User = require('../models/user.js')
+const db = require('../db')
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken")
+
+db.on('error', console.error.bind(console, 'MongoDB connection error'))
+
 // Change Password
 // Get
 // Get One
