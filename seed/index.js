60dbf599ca7e04137b4d1cd945b3cc601bd77b00
wjb108 <williambenedict3@gmail.com>
2021-04-16 14:48:34 -0400

    added password_digest field

--- seed/index.js
+++ seed/index.js
@@ -16,6 +16,7 @@ const main = async () => {
       programmingLanguage: ["react", "node", "python"],
       description:
         "i'm a few years out from the bootcamp and happy to help others in their journey succeed",
+      password_digest: "empty",
     },
     {
       username: "michaeljones",
@@ -23,6 +24,7 @@ const main = async () => {
       hourlyRate: "23",
       programmingLanguage: ["ruby", "html", "css"],
       description: "i'm not in it for the money here to share what i know",
+      password_digest: "empty",
     },
     {
       username: "tigerpines",
@@ -30,6 +32,7 @@ const main = async () => {
       hourlyRate: "34",
       programmingLanguage: ["ruby", "django", "php"],
       description: "i'm an experienced developer",
+      password_digest: "empty",
     },
   ];
 
@@ -37,14 +40,17 @@ const main = async () => {
     {
       username: "donaldjduck",
       email: "putitonmybill@gmail.com",
+      password_digest: "empty",
     },
     {
       username: "maverick",
       email: "dallassun@gmail.com",
+      password_digest: "empty",
     },
     {
       username: "fridayblues",
       email: "fridayyeah@gmail.com",
+      password_digest: "empty",
     },
   ];
 
