ffe11a0f74c72b1d178a7061b0184c49325759fa
Francis N <gundammerlin@gmail.com>
2021-04-16 12:11:29 -0500

    Model Revision 1

--- models/tutor.js
+++ models/tutor.js
@@ -6,7 +6,7 @@ const tutorSchema = new Schema(
     username: {type: String, required: true},
     email: {type: String, required: true},
     hourlyRate: {type: String, required: true},
-    programmingLanguage:{type: String, required: true}  ,
+    programmingLanguage: [type: String, required: true],
     description: {type: String, required: true},
     password_digest: { type: String, required: true }
   },
