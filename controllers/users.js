c8eadfbb2b9797ac46afbc96672e167e79b783c9
java80 <iekedede@hotmail.com>
2021-04-17 16:40:45 -0400

    Changes to user controller

--- controllers/users.js
+++ controllers/users.js
@@ -8,12 +8,7 @@ db.on('error', console.error.bind(console, 'MongoDB connection error'))
 const SALT_ROUNDS = 11;
 const TOKEN_KEY = "thebestgroupever";
 const signUp = async (req, res) => {
-  // take in user data
-  // set up password digest
-  // create a new user
-  // create a payload jwt
-  // create jwt token
-  // send back token
+  
   try {
     const { username, email, password } = req.body;
     const password_digest = await bcrypt.hash(password, SALT_ROUNDS);
@@ -21,10 +16,11 @@ const signUp = async (req, res) => {
     await user.save();
     const payload = {
       username: user.username,
+      email: user.email
 
     };
     const token = jwt.sign(payload, TOKEN_KEY);
-    return res.status(201).json({ token, payload });
+    return res.status(201).json({ token});
   } catch (error) {
     return res.status(400).json({ error: error.message });
   }
