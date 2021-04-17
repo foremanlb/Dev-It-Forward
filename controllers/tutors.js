9b1ee295098118adc477edbfeef22085954d2622
Craig Navarro <craignavarro@Craigs-MacBook-Pro.local>
2021-04-17 12:23:19 -0700

    tutor sign up controller done

--- controllers/tutors.js
+++ controllers/tutors.js
@@ -1,7 +1,8 @@
 const Tutor = require('../models/tutor.js')
 const db = require('../db')
 const bcrypt = require("bcrypt");
-const jwt = require("jsonwebtoken")
+const jwt = require("jsonwebtoken");
+const user = require('../models/user.js');
 
 db.on('error', console.error.bind(console, 'MongoDB connection error'))
 
@@ -20,5 +21,27 @@ const verify = async (req, res) => {
     
   } catch (error) {
     res.status(401).send("Validation Error");
+  }
+}
+
+//Sign Up
+const signUp = async (req, res) => {
+  try {
+    const { username, email, hourlyRate, programmingLanguage, description, password } = req.body
+    const password_digest = await bcrypt.hash(password, SALT_ROUNDS)
+    
+    const tutor = new Tutor({ username, email, hourlyRate, programmingLanguage, description, password_digest })
+    
+    await tutor.save()
+    const payload = {
+      username: tutor.username,
+      email: tutor.email
+    }
+
+    const token = jwt.sign(payload, TOKEN_KEY)
+
+    return res.status(201).json({token})
+  } catch (error) {
+    return res.status(400).json({error: error.message})
   }
 }
