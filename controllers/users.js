180a22bc41c90b76d5ce289bf64aadd8a0f1c0dd
Mobile <gundammerlin@gmail.com>
2021-04-17 16:37:59 -0500

    added User. to await

--- controllers/users.js
+++ controllers/users.js
@@ -36,7 +36,7 @@ const getUser = async (req, res) => {
 const updateUser = async (req, res) => {
     try {
         const { id } = req.params;
-        const updatedUser = await findByIdAndUpdate (id, req.body, {new: true});
+        const updatedUser = await User.findByIdAndUpdate (id, req.body, {new: true});
         return res.status(200).json(updatedUser)
     } catch (error) {
         return res.status(500).json({error: error.message});
