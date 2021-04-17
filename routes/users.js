e66aadeb7d2c34053479c4fed8ade9861fb796e2
Mobile <gundammerlin@gmail.com>
2021-04-17 17:35:55 -0500

    User Controllers with Restrict

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,12 @@
 const { Router} = require("express");
-const { signUp } = require("../controllers/users.js");
+const { getUsers, getUser, deleteUser, changePassword, signUp } = require("../controllers/users.js");
 const router = Router();
-router.post("/sign-up", signUp);
+
+
+
+
+
+
+
+
 module.exports = router; 
