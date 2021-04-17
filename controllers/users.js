ee72cfde1c8014b8f1f7c7cc8812035818568b3e
Mobile <gundammerlin@gmail.com>
2021-04-17 16:09:53 -0500

    reconciled Will's review 2

--- controllers/users.js
+++ controllers/users.js
@@ -25,7 +25,7 @@ const getUser = async (req, res) => {
     try {
         const { id } = req.params;
         const user = await User.findById(id);
-        res.status(200).json(user)
+        return res.status(200).json(user)
     } catch (error) {
         res.status(500).json({error: error.message});
     }
