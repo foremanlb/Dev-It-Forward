98a1647544f2341a8f7440a42b3bc90e88f5a187
java80 <iekedede@hotmail.com>
2021-04-17 15:35:19 -0400

    User signUp controller done

--- controllers/users.js
+++ controllers/users.js
@@ -5,3 +5,32 @@ const jwt = require("jsonwebtoken")
 
 
 db.on('error', console.error.bind(console, 'MongoDB connection error'))
+const SALT_ROUNDS = 11;
+const TOKEN_KEY = "thebestgroupever";
+const signUp = async (req, res) => {
+  // take in user data
+  // set up password digest
+  // create a new user
+  // create a payload jwt
+  // create jwt token
+  // send back token
+  try {
+    const { username, email, password } = req.body;
+    const password_digest = await bcrypt.hash(password, SALT_ROUNDS);
+    const user = new User({ username, email, password_digest });
+    await user.save();
+    const payload = {
+      username: user.username,
+
+    };
+    const token = jwt.sign(payload, TOKEN_KEY);
+    return res.status(201).json({ token, payload });
+  } catch (error) {
+    return res.status(400).json({ error: error.message });
+  }
+
+};
+
+module.exports = {
+  signUp
+};
