07b1d1be468f1ed73e8357dc5e5f3daf1975eaf5
java80 <iekedede@hotmail.com>
2021-04-18 12:12:23 -0400

    Get a single Tutor done

--- controllers/tutors.js
+++ controllers/tutors.js
@@ -13,7 +13,21 @@ const getTutors = async (req, res) => {
     const tutors = await Tutor.find({});
     res.status(200).json(tutors);
   } catch (error) {
-    return res.status(500).json({ error: error.message })
+    return res.status(500).json({ error: error.message });
+  }
+};
+// Get a Single tutor
+const getTutor = async (req, res) => {
+  try {
+    const { id } = req.params;
+    let tutor = await Tutor.findById(id);
+    if (tutor) {
+      return res.status(200).json(tutor);
+    } else {
+      return res.status(404).json({ error: " No tutor found" });
+    }
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
   }
 };
 //Verify
