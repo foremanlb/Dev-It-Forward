e7dad4f6cb34fbafa6090ab8979688c2e21811ab
wjb108 <williambenedict3@gmail.com>
2021-04-18 12:46:18 -0400

    added verify route in user.js file

--- routes/users.js
+++ routes/users.js
@@ -1,16 +1,22 @@
-const { Router} = require("express");
-const { getUsers, getUser, deleteUser, changePassword, signUp, updateUser } = require("../controllers/users.js");
+const { Router } = require("express");
+const {
+  getUsers,
+  verify,
+  getUser,
+  deleteUser,
+  changePassword,
+  signUp,
+  updateUser,
+} = require("../controllers/users.js");
 const restrict = require("../helpers/restrict.js");
 const router = Router();
 
-router.post('/sign-up',signUp)
-router.get('/',getUsers);
-router.get('/:id',getUser);
-router.put('/password-change/:id',changePassword);
-router.put('/:id', restrict, updateUser);
-router.delete('/:id',restrict,deleteUser);
+router.post("/sign-up", signUp);
+router.get("/", getUsers);
+router.get("/:id", getUser);
+router.get("/verify", verify);
+router.put("/password-change/:id", changePassword);
+router.put("/:id", restrict, updateUser);
+router.delete("/:id", restrict, deleteUser);
 
-
-
-
-module.exports = router; 
+module.exports = router;
