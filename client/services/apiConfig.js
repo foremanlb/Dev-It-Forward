5ca22f0ce8a5bbbc175363a5698a5fed9b3731a2
wjb108 <williambenedict3@gmail.com>
2021-04-19 18:14:09 -0400

    api added to path

--- client/services/apiConfig.js
+++ client/services/apiConfig.js
@@ -9,7 +9,7 @@ const getToken = async () => {
 let apiUrl;
 
 const apiUrls = {
-  production: "https://dev-it-forward-db.herokuapp.com/",
+  production: "https://dev-it-forward-db.herokuapp.com/api",
   development: "http://localhost:4567/api",
 };
 
