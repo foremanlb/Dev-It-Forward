accd887028adced909be45a22bac65750f50551b
java80 <iekedede@hotmail.com>
2021-04-19 18:12:47 -0400

    Added sign-in and sign-up path to app .js

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import { verifyUser } from "./services/user.js";
+import { Switch, Route } from "react-router-dom";
 
 function App() {
+   const [currentUser, setCurrentUser] = useState(null);
+
+  // const logOut = async () => {
+  //   await localStorage.clear();
+  //   setCurrentUser(null);
+    
+  // };
+  // useEffect(() => {
+  //   requestVerification();
+  // }, []);
+  // const requestVerification = async () => {
+  //   const user = await verifyUser();
+  //   setCurrentUser(user)
+  // };
+
   return (
     <div className="App">
+      <switch>
+        <Route exact path="/">
+          
+        </Route>
+        <Route path="/sign-up">
+          
+        </Route>
+        <Route path="/sign-in">
+          
+      </Route>
+
+
+      </switch>
       
     </div>
   );
