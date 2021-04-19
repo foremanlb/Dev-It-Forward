2fa51ef58a6b7cfdeb5ed244b6f2b269d6d29a53
wjb108 <williambenedict3@gmail.com>
2021-04-19 11:52:43 -0400

    added signin

--- routes/tutors.js
+++ routes/tutors.js
@@ -1,13 +1,22 @@
-const { Router } = require('express')
-const { signUp,getTutors,getTutor,updateTutor,deleteTutor,changePassword } = require("../controllers/tutors.js")
-const restrict = require("../helpers/restrict.js")
+const { Router } = require("express");
+const {
+  signUp,
+  signIn,
+  getTutors,
+  getTutor,
+  updateTutor,
+  deleteTutor,
+  changePassword,
+} = require("../controllers/tutors.js");
+const restrict = require("../helpers/restrict.js");
 
-const router = Router()
-router.put('/password-change/:id',changePassword);
+const router = Router();
+router.put("/password-change/:id", changePassword);
 router.get("/", getTutors);
 router.get("/", getTutor);
 router.put("/:id", restrict, updateTutor);
 router.delete("/:id", restrict, deleteTutor);
-router.post('/sign-up', signUp)
+router.post("/sign-up", signUp);
+router.post("/sign-in", signIn);
 
 module.exports = router;
