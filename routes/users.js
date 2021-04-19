a9bccefdfa31dfb9c83332eff41187301b411c0d
wjb108 <williambenedict3@gmail.com>
2021-04-19 10:40:46 -0400

    committing changes

--- routes/users.js
+++ routes/users.js
@@ -11,7 +11,6 @@ const {
 const restrict = require("../helpers/restrict.js");
 const router = Router();
 
-<<<<<<< HEAD
 router.post("/sign-up", signUp);
 router.get("/", getUsers);
 router.get("/:id", getUser);
@@ -19,13 +18,5 @@ router.get("/verify", verify);
 router.put("/password-change/:id", changePassword);
 router.put("/:id", restrict, updateUser);
 router.delete("/:id", restrict, deleteUser);
-=======
-router.post('/sign-up', signUp)
-router.get('/', getUsers);
-router.get('/:id', getUser);
-router.put('/password-change/:id', changePassword);
-router.put('/:id', restrict, updateUser);
-router.delete('/:id', restrict, deleteUser);
->>>>>>> d5c74b69d21f432de19944f517341eb0730dd33c
 
 module.exports = router;
