1f941c9ce9fc01e36ef2c47ee356ba25d3542cf9
java80 <iekedede@hotmail.com>
2021-04-20 05:31:15 -0400

    SignIn export function done

--- client/services/users.js
+++ client/services/users.js
@@ -2,7 +2,17 @@ import api from "./apiConfig";
 export const signUp = async (Credential) => {
   try {
     const resp = await api.post('/sign-up', Credential);
-    localStorage.setItem("token",resp.data.token)
+    localStorage.setItem("token", resp.data.token)
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const signIn = async (Credential) => {
+  try {
+    const resp = await api.post('/sign-in', Credential)
+    localStorage.setItem("token", resp.data.token)
+    return resp.data
   } catch (error) {
     throw error;
   }
