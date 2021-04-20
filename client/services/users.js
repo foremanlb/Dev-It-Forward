d8f2cd09ee1f17ecbafbdbe5e92dc0895f2f5f30
java80 <iekedede@hotmail.com>
2021-04-20 11:41:47 -0400

    getUser done

--- client/services/users.js
+++ client/services/users.js
@@ -1,7 +1,30 @@
 import api from "./apiConfig";
+
+
+
+export const getUsers = async () => {
+  try {
+    const res = await api.get("/users")
+    return res.data;
+  } catch (error) {
+    throw error
+  }
+
+  
+}
+
+export const getUser = async (id) => {
+  try {
+    let res = await api.get(`/users/${id}`)
+    return res.data
+  } catch (error) {
+    
+  }
+ 
+}
 export const signUp = async (Credential) => {
   try {
-    const resp = await api.post("/sign-up", Credential);
+    const resp = await api.post("/users/sign-up",Credential);
     localStorage.setItem("token", resp.data.token);
   } catch (error) {
     throw error;
@@ -10,7 +33,7 @@ export const signUp = async (Credential) => {
 
 export const signIn = async (Credential) => {
   try {
-    const resp = await api.post("/sign-in", Credential);
+    const resp = await api.post("/users/sign-in", Credential);
     localStorage.setItem("token", resp.data.token);
     return resp.data;
   } catch (error) {
@@ -21,7 +44,7 @@ export const signIn = async (Credential) => {
 export const verifyUser = async () => {
   const token = await localStorage.getItem("token");
   if (token) {
-    const res = await api.get("/verify");
+    const res = await api.get("/users/verify");
     return res.data;
   } else {
     return false;
