f4817996c81186da0b605e8dc557ae69f260cb6d
wjb108 <williambenedict3@gmail.com>
2021-04-19 23:08:07 -0400

    landing page screen

---
+++ client/screens/Landing/index.js
@@ -0,0 +1,64 @@
+import { sigIn } from "../../services/users.js";
+import { useState } from "react";
+import { Redirect } from "react-router-dom";
+
+export default function Landing() {
+  let defaultInput = {
+    username: "",
+    password: "",
+  };
+  const [input, setInput] = useState(defaultInput);
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    await sigIn(input);
+    <Redirect to="/tutors"></Redirect>;
+    // do i need verify here as well?
+  };
+
+  function handleClick(event) {
+    <Redirect to="/sign-up"></Redirect>;
+  }
+
+  function handleChange(event) {
+    let { id, value } = event.target;
+    setInput((prevInput) => ({
+      ...prevInput,
+      [id]: value,
+    }));
+  }
+
+  return (
+    <div>
+      <h1 className="hero-image-header-text">Level up with a coding tutor</h1>
+      <div className="hero-image-container">
+        <img
+          src="https://images.pexels.com/photos/3861964/pexels-photo-3861964.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
+          alt="image"
+        />
+      </div>
+      <p className="hero-image-paragraph-text">
+        1-on-1 coaching from someone that has gone through the same bootcamp you
+        are going through. Debugging, career advice, and much more.
+      </p>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="username" placeholder="username"></label>
+        <input
+          type="text"
+          id="username"
+          onChange={handleChange}
+          value={input.username}
+        ></input>
+        <label htmlFor="password" placeholder="password"></label>
+        <input
+          type="text"
+          id="password"
+          onChange={handleChange}
+          value={input.password}
+        ></input>
+        <button type="submit">Sign-In</button>
+      </form>
+      <button onClick={handleClick}>Sign-Up</button>
+    </div>
+  );
+}
