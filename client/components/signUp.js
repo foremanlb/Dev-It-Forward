3281af5c1b4d38bd988f9637c6951c5e50856a22
java80 <iekedede@hotmail.com>
2021-04-20 17:09:38 -0400

    Added user sign up

---
+++ client/components/signUp.js
@@ -0,0 +1,60 @@
+import { useState } from "react";
+
+import { SignUp } from "../services/users"
+
+export default function SignUp(props) {
+  const defaultInput = {
+    username: "",
+    email: "",
+    password: ""
+   
+  };
+
+  const [input, setInput] = useState(defaultInput);
+  
+  const handleChange = (event) => {
+    let { name, value } = event.target;
+    setInput((prevInput) => ({
+      ...prevInput, [name]: value,
+
+    }));
+}
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    await signUp(input);
+    let res = await signIn(input);
+    //
+ }
+return (
+  <div>
+    <h2> Header Student </h2>
+    <form onChange = {handleChange}>
+      <label>username</label>
+      <input
+        name="username"
+        value={input.username}
+        placeholder = "Enter username .."
+      />
+      <label>Email</label>
+      <input
+        name="email"
+        type = "email"
+        value={input.email}
+        placeholder = "Enter email .."
+      />
+
+      <label>Password</label>
+      <input
+        name="password"
+        type ="password"
+        value={input.password}
+        placeholder = "Enter password .."
+      />
+      <button type = "submit"></button>
+
+
+    </form>
+  </div>
+)
+  
+}
