f2458cd2a9844bc5c3515c0416fd4bbc292c3998
java80 <iekedede@hotmail.com>
2021-04-20 14:51:40 -0400

    Student header added

--- client/screens/signUp.js
+++ client/screens/signUp.js
@@ -11,11 +11,50 @@ export default function SignUp(props) {
 
   const [input, setInput] = useState(defaultInput);
   
+  const handleChange = (event) => {
+    let { name, value } = event.target;
+    setInput((prevInput) => ({
+      ...prevInput, [name]: value,
 
+    }));
+}
 
 return (
   <div>
-   
+    <h2> Student </h2>
+    <form>
+      <label>username</label>
+      <input
+        name="username"
+        value={input.username}
+        placeholder = "Enter username .."
+      />
+      <label>Email</label>
+      <input
+        name="email"
+        type = "email"
+        value={input.email}
+        placeholder = "Enter email .."
+      />
+
+      <label>Password</label>
+      <input
+        name="password"
+        type ="password"
+        value={input.password}
+        placeholder = "Enter password .."
+      />
+       <label>Password Confirmation</label>
+      <input
+        name="password"
+        type = "password"
+        value={input.password}
+        placeholder = "Enter password .."
+      />
+      <button type = "submit"></button>
+
+
+    </form>
   </div>
 )
   
