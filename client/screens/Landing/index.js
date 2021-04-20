8360572b5b155453ee1c3819e27cef98863e01ac
wjb108 <williambenedict3@gmail.com>
2021-04-19 23:11:02 -0400

    added classname

--- client/screens/Landing/index.js
+++ client/screens/Landing/index.js
@@ -33,6 +33,7 @@ export default function Landing() {
       <h1 className="hero-image-header-text">Level up with a coding tutor</h1>
       <div className="hero-image-container">
         <img
+          className="hero-image"
           src="https://images.pexels.com/photos/3861964/pexels-photo-3861964.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
           alt="image"
         />
