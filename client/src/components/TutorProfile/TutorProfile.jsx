352681d358d8c5e3159128564ac944386b685246
Craig Navarro <craignavarro@Craigs-MacBook-Pro.local>
2021-04-21 12:24:56 -0700

    fixing file names

---
+++ client/src/components/TutorProfile/TutorProfile.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import { deleteTutor } from '../../services/tutors'
+import {useHistory} from "react-router-dom"
+
+export default function TutorProfile(props) {
+  const tutor = props.tutor
+  let history = useHistory();
+  
+  // async function handleDelete() {
+  //   const deletedTutor = await deleteTutor(tutor._id)
+  //   history.push("/")
+  //   }
+
+  return (
+    <div>
+      <h1>hello</h1>
+      {/* <h3>{tutor.username}</h3>
+      <h5>{tutor.email}</h5>
+      <p>{`Hourly Rate: $${tutor.hourlyRate}`}</p>
+      <ul>{tutor.programmingLanguage.map((language) => {
+        return (
+          <li key={language}>
+            {language}
+          </li>
+        )
+      })}
+      </ul>
+      <p>{tutor.description}</p>
+      <button className="delete-tutor" onClick={handleDelete}></button> */}
+    </div>
+
+  )
+}
