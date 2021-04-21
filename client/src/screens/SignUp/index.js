ce45abd0e40e3b3778f36009a683d98c2b20ab33
Craig Navarro <craignavarro@Craigs-MacBook-Pro.local>
2021-04-21 12:17:36 -0700

    fixed file paths

--- client/src/screens/SignUp/index.js
+++ client/src/screens/SignUp/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 // import UserSignUp from '../components/UserSignUp.jsx'
-import TutorSignUp from "../../components/tutorSignUp/TutorSignUp";
+import TutorSignUp from "../../components/TutorSignUp/TutorSignUp";
 
 export default function SignUp() {
   return (
