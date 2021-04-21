2a618198a95c7a2417fd7c6c2ab660612d4780a4
wjb108 <williambenedict3@gmail.com>
2021-04-21 18:00:48 -0400

    new changes

--- client/src/components/TutorCard/TutorCard.jsx
+++ client/src/components/TutorCard/TutorCard.jsx
@@ -1,22 +1,20 @@
-import React from 'react'
+import { Link } from "react-router-dom";
 
 export default function TutorCard(props) {
-  const tutor = props.tutor
+  const tutor = props.tutor;
 
   return (
     <div>
       <h3>{tutor.username}</h3>
       <h5>{tutor.email}</h5>
       <p>{`Hourly Rate: $${tutor.hourlyRate}`}</p>
-      <ul>{tutor.programmingLanguage.map((language) => {
-        return (
-          <li key={language}>
-            {language}
-          </li>
-        )
-      })}
+      <ul>
+        {tutor.programmingLanguage.map((language) => {
+          return <li key={language}>{language}</li>;
+        })}
       </ul>
       <p>{tutor.description}</p>
+      <Link to={`/details/${tutor._id}`}>Details</Link>
     </div>
-  )
+  );
 }
---
+++ client/src/screens/Details/index.js
@@ -0,0 +1,9 @@
+import TutorDetails from "../../components/TutorDetail/TutorDetail.jsx";
+
+export default function Details() {
+  return (
+    <div>
+      <TutorDetails />
+    </div>
+  );
+}
