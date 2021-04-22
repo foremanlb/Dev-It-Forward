68feae5fed39a81c8a8358be67131eddf4266696
wjb108 <williambenedict3@gmail.com>
2021-04-22 13:29:50 -0400

    styling

--- client/src/screens/TutorGallery/index.js
+++ client/src/screens/TutorGallery/index.js
@@ -1,28 +1,31 @@
-import React from 'react'
-import { Redirect } from 'react-router-dom'
-import CardContainer from '../../components/CardContainer/CardContainer'
+import React from "react";
+import { Redirect } from "react-router-dom";
+import CardContainer from "../../components/CardContainer/CardContainer";
+import "./index.css";
 
 export default function TutorGallery(props) {
-  const currentUser = props.currentUser
-  const currentTutor = props.currentTutor
-  const tutors = props.tutors
+  const currentUser = props.currentUser;
+  const currentTutor = props.currentTutor;
+  const tutors = props.tutors;
 
   const renderPage = () => {
-    if (currentUser || currentTutor ) {
+    if (currentUser || currentTutor) {
       return (
         <div>
-          <CardContainer tutors={tutors}/>
+          <CardContainer tutors={tutors} />
         </div>
-      )
+      );
     } else {
-      return <Redirect to='/Landing' />
+      return <Redirect to="/Landing" />;
     }
-  }
+  };
 
   return (
     <div>
-      <button onClick={props.logout}>Sign Out</button>
+      <button className="sign-out-button" onClick={props.logout}>
+        Sign Out
+      </button>
       {renderPage()}
     </div>
-  )
+  );
 }
