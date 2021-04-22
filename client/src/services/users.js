e3db1ceec10be8f212e6c8db5d8cf0af400a5890
wjb108 <williambenedict3@gmail.com>
2021-04-22 18:13:24 -0400

    user profile added style

--- client/src/services/users.js
+++ client/src/services/users.js
@@ -1,41 +1,39 @@
-
 import api from "./apiConfig";
 export const getUsers = async () => {
   try {
-    const res = await api.get("/users")
-    return res.data;
-  } catch (error) {
-    throw error
-  }
-}
-export const getUser = async (id) => {
-  try {
-    let res = await api.get(`/users/user/${id}`)
-    return res.data
-  } catch (error) {
-    throw error
-  }
- 
-}
-export const updatedUser = async (id,user) => {
-  try {
-    let res = await api.put(`/users/${id}`, user)
+    const res = await api.get("/users");
     return res.data;
   } catch (error) {
     throw error;
   }
-}
+};
+export const getUser = async (id) => {
+  try {
+    let res = await api.get(`/users/user/${id}`);
+    return res.data;
+  } catch (error) {
+    throw error;
+  }
+};
+export const updatedUser = async (id, user) => {
+  try {
+    let res = await api.put(`/users/${id}`, user);
+    return res.data;
+  } catch (error) {
+    throw error;
+  }
+};
 export const deleteUser = async (id) => {
-  const token = await localStorage.get("token")
+  const token = await localStorage.getItem("token");
   if (token) {
-   const deletedUser = await api.delete(`/users/${id}`);
+    const deletedUser = await api.delete(`/users/${id}`);
     return deletedUser;
   }
-}
+};
 
 export const signUpUser = async (credentials) => {
   try {
-    const resp = await api.post("/users/sign-up",credentials);
+    const resp = await api.post("/users/sign-up", credentials);
     localStorage.setItem("token", resp.data.token);
   } catch (error) {
     throw error;
@@ -62,12 +60,9 @@ export const verifyUser = async () => {
   }
 };
 export const changeUserPassword = async () => {
-  const token = await localStorage.getItem('token');
+  const token = await localStorage.getItem("token");
   if (token) {
     const res = await api.put("/users/password-change/:id");
-    return res.data
-  }
-  else
-    return false
-  
-}
+    return res.data;
+  } else return false;
+};
