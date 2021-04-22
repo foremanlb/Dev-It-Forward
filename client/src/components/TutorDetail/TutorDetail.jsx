147e802ceb47a8305b7a994db4934e559cb1b100
java80 <iekedede@hotmail.com>
2021-04-22 16:07:06 -0400

    Added radius to tutor profile image

--- client/src/components/TutorDetail/TutorDetail.jsx
+++ client/src/components/TutorDetail/TutorDetail.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import './TutorDetail.css'
 import { getTutor } from "../../services/tutors";
 import { useParams } from "react-router-dom";
-
+import hero from '../../images/hero.jpeg'
 const TutorDetail = (props) => {
   const [tutor, setTutor] = useState({});
   const { id } = useParams();
@@ -19,10 +19,11 @@ const TutorDetail = (props) => {
   return (
     <div className="tutor-detail">
       <div className="detail">
+      <div className="tutor-user-image"><img className="hero-image" src={hero} alt="laptop"/></div>
       <div className="tutor-user-name"> Tutor:  {tutor.username}</div>
         <div className="tutor-email"> Email: {tutor.email}</div>
         <div className="tutor-hourlyrate"> Hourly Rate: {tutor.hourlyRate}</div>
-        <ul>
+        <ul className = "language-list">
           {tutor.programmingLanguage &&
             tutor.programmingLanguage.map((language) => {
               return <li key={language}>{language}</li>;
