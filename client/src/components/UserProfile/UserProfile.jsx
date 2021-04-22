1e29414d25b154c5725d37d8cc83b026516a5193
wjb108 <williambenedict3@gmail.com>
2021-04-22 16:23:51 -0400

    pushed

--- client/src/components/UserProfile/UserProfile.jsx
+++ client/src/components/UserProfile/UserProfile.jsx
@@ -1,60 +1,61 @@
 import { deleteUser, updatedUser } from "../../services/users";
 import { useHistory } from "react-router-dom";
-import {useState} from 'react'
+import { useState } from "react";
 
 export default function UserProfile(props) {
-  const user = props.user
-  const [input, setInput] = useState(user)
+  const user = props.user;
+  const [input, setInput] = useState(user);
   let history = useHistory();
-  const setToggle = props.setToggle
+  const setToggle = props.setToggle;
 
   async function handleDelete() {
-    await deleteUser(user._id)
+    await deleteUser(user._id);
     await localStorage.clear();
-    setToggle((prevState) => !prevState)
-    history.push("/")
+    setToggle((prevState) => !prevState);
+    history.push("/");
   }
 
   const handleChange = (e) => {
-    let { name, value } = e.target
+    let { name, value } = e.target;
     setInput((prevInput) => ({
       ...prevInput,
       [name]: value,
-    }))
-  }
+    }));
+  };
 
   const handleSubmit = async (e) => {
-    e.preventDefault()
-    await updatedUser(user._id, input)
-    setToggle((prevState) => !prevState)
-  }
+    e.preventDefault();
+    await updatedUser(user._id, input);
+    setToggle((prevState) => !prevState);
+  };
 
   return (
     <div>
       <h3>{`Nice to see you again, ${user.username}`}</h3>
       <p>This is the info we have on your profile</p>
       <form onSubmit={handleSubmit}>
-      <label htmlFor='username'>User Name</label>
+        <label htmlFor="username">User Name</label>
         <input
-          type='text'
-          name='username'
+          type="text"
+          name="username"
           required
           onChange={handleChange}
           value={input.username}
-          placeholder='hello'
+          placeholder="hello"
         />
-        <label htmlFor='email'>Email</label>
+        <label htmlFor="email">Email</label>
         <input
-          type='email'
-          name='email'
+          type="email"
+          name="email"
           required
           onChange={handleChange}
           value={input.email}
         />
-        <button type='submit'>Update Profile</button>
-        </form>
-      <button className="delete-user" onClick={handleDelete}>Delete Profile</button>
+        <button type="submit">Update Profile</button>
+      </form>
+      <button className="delete-user" onClick={handleDelete}>
+        Delete Profile
+      </button>
     </div>
   );
 }
-
